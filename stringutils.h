ea17b0f67d0a2fd12330ba79a780040fad084c01
Sebastian Rettenberger <rettenbs@in.tum.de>
2013-11-08 15:13:31 +0100

    Added trim functions

--- stringutils.h
+++ stringutils.h
@@ -13,6 +13,10 @@
 #ifndef UTILS_STRINGUTILS_H
 #define UTILS_STRINGUTILS_H
 
+#include <algorithm>
+#include <functional>
+#include <cctype>
+#include <locale>
 #include <sstream>
 #include <string>
 
@@ -51,6 +55,35 @@ public:
 		std::ostringstream ss;
 		ss << value;
 		return ss.str();
+	}
+
+	/**
+	 * Trims from start
+	 *
+	 * Taken from http://stackoverflow.com/questions/216823/whats-the-best-way-to-trim-stdstring
+	 */
+	static std::string &ltrim(std::string &s) {
+	        s.erase(s.begin(), std::find_if(s.begin(), s.end(), std::not1(std::ptr_fun<int, int>(std::isspace))));
+	        return s;
+	}
+
+	/**
+	 * Trims from end
+	 *
+	 * Taken from http://stackoverflow.com/questions/216823/whats-the-best-way-to-trim-stdstring
+	 */
+	static std::string &rtrim(std::string &s) {
+	        s.erase(std::find_if(s.rbegin(), s.rend(), std::not1(std::ptr_fun<int, int>(std::isspace))).base(), s.end());
+	        return s;
+	}
+
+	/**
+	 * Trims from both ends
+	 *
+	 * Taken from http://stackoverflow.com/questions/216823/whats-the-best-way-to-trim-stdstring
+	 */
+	static std::string &trim(std::string &s) {
+	        return ltrim(rtrim(s));
 	}
 };
 
