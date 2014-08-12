39f361f83875f7af03b6c354bbc7e9d77e506ead
Sebastian Rettenberger <rettenbs@in.tum.de>
2014-08-12 22:30:19 +0200

    Add replaceLast function

--- stringutils.h
+++ stringutils.h
@@ -43,14 +43,26 @@ namespace utils
 class StringUtils
 {
 public:
+
 	/**
-	 * Replaces from in str with to
+	 * Replaces first occurrence of from in str with to
 	 *
 	 * Taken from http://stackoverflow.com/questions/3418231/c-replace-part-of-a-string-with-another-string
 	 */
 	static bool replace(std::string& str, const std::string& from, const std::string& to) {
 		size_t start_pos = str.find(from);
 		if(start_pos == std::string::npos)
+			return false;
+		str.replace(start_pos, from.length(), to);
+		return true;
+	}
+
+	/**
+	 * Replaces last occurrence of from in str with to
+	 */
+	static bool replaceLast(std::string& str, const std::string& from, const std::string& to) {
+		size_t start_pos = str.rfind(from);
+		if (start_pos == std::string::npos)
 			return false;
 		str.replace(start_pos, from.length(), to);
 		return true;
