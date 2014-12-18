1e2ff49ecdccf13c0957ad960f4c80579ac5a05d
Sebastian Rettenberger <rettenbs@in.tum.de>
2014-12-18 13:22:22 +0100

    Add path class

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 CC=g++
 CFLAGS=-c -I..
-SOURCES=args.h env.h logger.h mathutils.h progress.h stringutils.h timeutils.h
+SOURCES=args.h env.h logger.h mathutils.h path.h progress.h stringutils.h timeutils.h
 OBJS=$(SOURCES:.h=.h.gch)
 
 all: $(OBJS)
---
+++ path.h
@@ -0,0 +1,103 @@
+/**
+ * @file
+ *  This file is part of UTILS
+ *
+ * UTILS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * UTILS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with UTILS.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @copyright 2014 Technische Universitaet Muenchen
+ * @author Sebastian Rettenberger <rettenbs@in.tum.de>
+ */
+
+#ifndef UTILS_PATH_H
+#define UTILS_PATH_H
+
+#include <string>
+
+#include "utils/stringutils.h"
+
+namespace utils
+{
+
+/**
+ * Manipulates file/directory names and paths
+ */
+class Path
+{
+private:
+	std::string m_path;
+
+public:
+	Path(const char* path)
+		: m_path(path)
+	{
+		init();
+	}
+
+	Path(const std::string &path)
+		: m_path(path)
+	{
+		init();
+	}
+
+	/**
+	 * @return The basename of the path
+	 */
+	std::string basename() const
+	{
+		const size_t lastSlash = m_path.find_last_of(separators());
+		if (lastSlash == std::string::npos)
+			return m_path;
+
+		return m_path.substr(lastSlash+1);
+	}
+
+public:
+	static const char* separator()
+	{
+		static const std::string sep(1, SEPARATOR);
+		return sep.c_str();
+	}
+
+	/**
+	 * @return A string containing all possible separators
+	 */
+	static const char* separators()
+	{
+#if __unix__
+		return "/";
+#else // __unix__
+		return "\\/";
+#endif // __unix__
+	}
+
+private:
+	void init()
+	{
+		// Remove trailing separator
+		if (StringUtils::endsWith(m_path, separator()))
+			StringUtils::replaceLast(m_path, separator(), "");
+	}
+
+public:
+#ifdef __unix__
+	static const char SEPARATOR = '/';
+#else // __unix__
+	static const char SEPARATOR = '\\';
+#endif // __unix__
+
+};
+
+}
+
+#endif // UTILS_PATH_H
