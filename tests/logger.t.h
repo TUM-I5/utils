b99f2527f7c92ff555b2f73b4cbb7dcc45489945
Sebastian Rettenberger <rettenbs@in.tum.de>
2014-12-18 15:00:29 +0100

    Add cxxtest and cmake

---
+++ tests/logger.t.h
@@ -0,0 +1,33 @@
+/**
+ * @file
+ *  This file is part of UTILS
+ *
+ * UTILS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * UTILS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with UTILS.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @copyright 2014 Technische Universitaet Muenchen
+ * @author Sebastian Rettenberger <rettenbs@in.tum.de>
+ */
+
+#include <cxxtest/TestSuite.h>
+
+#include "utils/logger.h"
+
+class TestLogger : public CxxTest::TestSuite
+{
+public:
+	void testFoo()
+	{
+		// TODO
+	}
+};
