987a116f4c48758a347c571f8760d297e16e4f2e
Travis <rettenbs@in.tum.de>
2015-01-23 15:15:22 +0000

    Travis build 22 pushed to gh-pages

--- search/all_62.js
+++ search/all_62.js
@@ -1,4 +1,5 @@
 var searchData=
 [
+  ['backtrace_5fsize',['BACKTRACE_SIZE',['../logger_8h.html#ae169958ccf4c1db90f44c1711ef74ce9',1,'logger.h']]],
   ['basename',['basename',['../classutils_1_1Path.html#a92fcfb119e5d13b11aefef81576ac418',1,'utils::Path']]]
 ];
---
+++ search/defines_62.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['backtrace_5fsize',['BACKTRACE_SIZE',['../logger_8h.html#ae169958ccf4c1db90f44c1711ef74ce9',1,'logger.h']]]
+];
