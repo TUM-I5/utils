82617148a5108ced6ccf77ebcab61e59b1d07a95
Travis <rettenbs@in.tum.de>
2017-06-26 11:02:51 +0000

    Travis build 39 pushed to gh-pages

--- search/all_72.js
+++ search/all_72.js
@@ -5,5 +5,6 @@ var searchData=
   ['required',['Required',['../classutils_1_1Args.html#ac7a42fde49a392c1d74e93d07594c4f6a50c6a9696a1d5a432b5deb3674024ceb',1,'utils::Args']]],
   ['result',['Result',['../classutils_1_1Args.html#a76f8c87011dc03bf54e5bd6d0bedc368',1,'utils::Args']]],
   ['rotation_5find',['ROTATION_IND',['../progress_8h.html#a41e5dc252c4a7785e2cab8f2d7952015',1,'progress.h']]],
+  ['roundup',['roundUp',['../classutils_1_1MathUtils.html#a31d4440740c8a09da9becd40fc286000',1,'utils::MathUtils']]],
   ['rtrim',['rtrim',['../classutils_1_1StringUtils.html#a7aacb88c5b78aba93eaeaac8e946a91d',1,'utils::StringUtils']]]
 ];
--- search/functions_72.js
+++ search/functions_72.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['replace',['replace',['../classutils_1_1StringUtils.html#af3983e9ee65d4056ca7749d6d2e5ca20',1,'utils::StringUtils']]],
   ['replacelast',['replaceLast',['../classutils_1_1StringUtils.html#a969ed1cbf12ac49f98b2f3f68381e3a5',1,'utils::StringUtils']]],
+  ['roundup',['roundUp',['../classutils_1_1MathUtils.html#a31d4440740c8a09da9becd40fc286000',1,'utils::MathUtils']]],
   ['rtrim',['rtrim',['../classutils_1_1StringUtils.html#a7aacb88c5b78aba93eaeaac8e946a91d',1,'utils::StringUtils']]]
 ];
